078779d09f1bedbb7a83cf220be9c3b50caede7a
mohammad Khamis <mohammad.khamis@repzoapp.com>
2020-03-19 14:42:25 +0200

    bug fixes

--- example/models/Employee.js
+++ example/models/Employee.js
@@ -2,21 +2,16 @@ var mongoose = require("mongoose");
 var config = require("../config");
 
 var EmployeeSchema = new mongoose.Schema({
-    name: {type: String},
-    dateOfBirth: {type: Date},
-    email: {type: String},
-    mobile: {type: String},
-    designation: {type: String},
-    employeeId: {type: String},
+  name: { type: String },
+  dateOfBirth: { type: Date },
+  email: { type: String },
+  mobile: { type: String },
+  designation: { type: String },
+  employeeId: { type: String }
 });
 
-var diffHistory = require("mongoose-diff-history/diffHistory").plugin;
+var diffHistory = require("mongoose-audit-trail").plugin;
 EmployeeSchema.plugin(diffHistory);
 
 var Employee = mongoose.model("Employee", EmployeeSchema);
 module.exports = Employee;
-
-
-
-
-
